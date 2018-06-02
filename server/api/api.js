2e53fd831c86416f94d236219b39eb85536c22bf
Daren Lunsford <dlunsfo1@asu.edu>
2018-06-02 00:46:26 -0500

    added circle CI integration

--- server/api/api.js
+++ server/api/api.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
 router.use('/cases', require('./case/caseRoutes'));
+// router.use('types', require('./type/typeRoutes'));
 
 module.exports = router;
---
+++ server/api/type/typeRoutes.js
@@ -0,0 +1,18 @@
+var router = require('express').Router();
+var controller = require('./typeController');
+
+//
+router.param('id', controller.param);
+
+router
+  .route('/')
+  .get(controller.get)
+  .post(controller.post);
+
+router
+  .route('/:id')
+  .get(controller.getOne)
+  .put(controller.put)
+  .delete(controller.delete);
+
+module.exports = router;
